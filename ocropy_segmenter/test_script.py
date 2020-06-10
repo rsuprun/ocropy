1389e536506c463b158892c38ab64f6eea9515d5
rsuprun <robin.suprun@fraserhealth.ca>
2020-06-09 19:07:30 -0700

    Initial commit of test script to help with method invocation

---
+++ ocropy_segmenter/test_script.py
@@ -0,0 +1,56 @@
+import os
+
+from ocropy_segmenter import segmenter
+import multiprocessing as mp
+
+
+def main_root_path():
+    _bp = os.getcwd()
+    for i in range(3):
+        _bp = os.path.split(_bp)[0]
+    assert os.path.exists(_bp)
+    _bp = os.path.join(_bp, 'NSQIP_Abstraction_Automation_Master', 'data_files', 'scanned_forms',
+                       'seg_test', 'pre_op_checklist_p1')
+    return _bp
+
+def find_new_files(src_path, dst_path, ftype, reprocess):
+    """
+    Find all files of ftype in the src_path that are not in the dst_path
+    :param src_path: str - full folder path of source files
+    :param dst_path: str - full folder path of destination files
+    :param ftype: str - '.filetype' - file type to search for with '.' included
+    :param reprocess: - boolean to set whether to process only new files or all ocropy_segmenter files
+    :return:
+    """
+
+    # collect the png files from the ocropy_segmenter folder
+    src_files = set([file for r, d, f in os.walk(src_path) for file in f if ftype in file])
+
+    if not reprocess:
+        # collect the png files from the dst folder
+        dst_files = set([file for r, d, f in os.walk(dst_path) for file in f if ftype in file])
+        # take only the ocropy_segmenter files not in the dst folder
+        src_files = src_files - dst_files
+
+    return [os.path.join(src_path, f) for f in src_files]
+
+
+def main():
+
+    rpath = main_root_path()
+    src_scan_path = os.path.join(rpath, 'cropped')
+    dest_scan_path = os.path.join(rpath, 'segged')
+
+    file_list = find_new_files(src_path=src_scan_path, dst_path=dest_scan_path, ftype='.png', reprocess=False)
+    file_dict = {os.path.splitext(os.path.split(f)[1])[0]:f for f in file_list}
+    total_files = len(file_list)
+    kwargs = {'minscale':6.0, 'pad': 15, 'threshold': 0.05, 'output': dest_scan_path, 'total_files':total_files}
+
+    pool = mp.Pool(mp.cpu_count())
+    pool.starmap(segmenter.main_process, [(file, kwargs, ind) for ind, (fname, file) in enumerate(file_dict.items())])
+    pool.close()
+    print('Finished')
+
+
+if __name__ == '__main__':
+    main()
