4b4d5629058f7f1c050c7399a0e24bd48f4a6d72
rsuprun <robin.suprun@fraserhealth.ca>
2020-06-09 19:11:01 -0700

    Refactor setup.py file

--- setup.py
+++ setup.py
@@ -1,33 +1,30 @@
-#!/usr/bin/env python
+from setuptools import setup, find_packages
 
-from __future__ import print_function
-
-import sys
-import glob
-import os.path
-from distutils.core import setup
-
-assert sys.version_info[0]==2 and sys.version_info[1]>=7,\
-    "you must install and use OCRopus with Python version 2.7 or later, but not Python 3.x"
-
-if not os.path.exists("models/en-default.pyrnn.gz"):
-    print()
-    print("You should download the default model 'en-default.pyrnn.gz'")
-    print("and put it into ./models.")
-    print()
-    print("Check https://github.com/tmbdev/ocropy for the location")
-    print("of model files.")
-    print()
-
-models = [c for c in glob.glob("models/*pyrnn.gz")]
-scripts = [c for c in glob.glob("ocropus-*") if "." not in c and "~" not in c]
 
 setup(
-    name = 'ocropy',
-    version = 'v1.3.3',
-    author = "Thomas Breuel",
-    description = "The OCRopy RNN-based Text Line Recognizer",
-    packages = ["ocrolib"],
-    data_files= [('share/ocropus', models)],
-    scripts = scripts,
-    )
+    name = 'ocropy_segmenter',
+    version = 'v0.1.0',
+    author = "Thomas Breuel (modified by Robin Suprun)",
+    description = "Simpliefied text segmentation from Ocropy package",
+    packages = find_packages(),
+    install_requires=["pillow>=7.1",
+                      "libopencv>=4.0",
+                      "numpy>=1.18",
+                      "scipy>=1.4",
+                      "matplotlib>=3.1",
+                      "imageio>=2.8"],
+
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Win32 (MS Windows)'
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Multimedia :: Graphics :: Capture',
+    ],
+
+)
+
+
